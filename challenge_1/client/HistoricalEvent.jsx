af3310cb5c9987438f89bfe4887d33f6b48db49f
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-25 11:45:39 -0700

    Add basic styling to historical events

--- challenge_1/client/HistoricalEvent.jsx
+++ challenge_1/client/HistoricalEvent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HistoricalEvent = (props) => (
-  <div>
+  <div className="historical-event">
     <div>{props.historicalEvent.date}</div>
     <div>{props.historicalEvent.description}</div>
   </div>
