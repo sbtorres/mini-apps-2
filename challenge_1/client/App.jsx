6c6942a1dd0c78d989a9f63fdfd98533800c4976
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-25 11:16:35 -0700

    Create axios get request for data based on query string

--- challenge_1/client/App.jsx
+++ challenge_1/client/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Search from './Search.jsx';
 
 class App extends React.Component {
@@ -10,7 +11,13 @@ class App extends React.Component {
   }
 
   getSearchResults(queryString) {
-    console.log(queryString);
+    axios.get(`http://localhost:3000/events?q=${queryString}`)
+      .then((results) => {
+        console.log(results);
+      })
+      .catch((err) => {
+        throw err;
+      });
   }
 
   render() {
