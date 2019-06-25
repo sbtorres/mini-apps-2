8a77cad10618a3a1f613488b11a43a160a4227ec
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-25 11:01:05 -0700

    Create App component, which will be parent component

--- challenge_1/client/index.jsx
+++ challenge_1/client/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.jsx';
 
-ReactDOM.render(<div>Test</div>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
