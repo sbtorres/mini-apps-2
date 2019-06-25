799d7cd623c2bac88ce7a78d2d736814f53e8dd2
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-25 11:07:45 -0700

    Create form and implement onChange handler to handle user input

--- challenge_1/client/Search.jsx
+++ challenge_1/client/Search.jsx
@@ -4,12 +4,28 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      search: '',
+    };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput (event) {
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      [name]: target.value
+    })
   }
 
   render() {
     return (
-      <div>Search Bar</div>
+      <form>
+        <label> Search: 
+          <input type="text" name="search" onChange={this.handleUserInput} />
+        </label>
+      </form>
     )
   }
 }
