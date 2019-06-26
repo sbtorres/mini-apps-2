0ac3aea0d89aef278bb60d64baa10330b5fa3db9
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-26 15:22:47 -0700

    Create onChange handler for start and end inputs

--- challenge_2/client/DateInputForm.jsx
+++ challenge_2/client/DateInputForm.jsx
@@ -4,7 +4,20 @@ class DateInputForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      start: '',
+      end: ''
+    };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput(event) {
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      [name]: target.value,
+    })
   }
 
   render() {
@@ -12,12 +25,12 @@ class DateInputForm extends React.Component {
       <div className="date-input-container">
         <form className="start-date-input">
           <label> Start Date: 
-            <input type="text" name="start" value={this.state.start} />
+            <input type="text" name="start" value={this.state.start} onChange={this.handleUserInput} />
           </label>
         </form>
         <form className="end-date-input">
           <label> End Date: 
-            <input type="text" name="start" value={this.state.start} />
+            <input type="text" name="end" value={this.state.end} onChange={this.handleUserInput} />
           </label>
         </form>
       </div>
