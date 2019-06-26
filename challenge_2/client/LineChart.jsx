99138600f829373845a2709419b8c3b496ace4cb
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-26 14:05:08 -0700

    Render basic chart to screen, not using live data

--- challenge_2/client/LineChart.jsx
+++ challenge_2/client/LineChart.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import Chart from 'chart.js';
 
 class LineChart extends React.Component {
   constructor(props) {
     super(props);
+    this.chartRef = React.createRef();
 
     this.state = {};
   }
 
+  componentDidMount() {
+    const myChartRef = this.chartRef.current.getContext("2d");
+
+    new Chart(myChartRef, {
+      type: "line",
+      data: {
+        labels: ["Jan", "Feb", "March"],
+        datasets: [
+            {
+                label: "BitCoin Historical Trend",
+                data: [1000.00, 1540.02, 2000.31],
+            }
+        ]
+      }
+    });
+  }
+
   render() {
-    return <div>Test</div>
+    return (
+      <div>
+        <canvas id="myChart" ref={this.chartRef} />
+      </div>
+    )
   }
 }
 
