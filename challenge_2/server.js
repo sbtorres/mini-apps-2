d65461b7d42b5fcbfa65011c8d1cd9955b9121bb
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-26 12:36:37 -0700

    Reduce amount of data being returned by server to only what is required

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const Axios = require('axios');
 
 const app = express();
 const PORT = 3000;
@@ -7,9 +7,9 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.get('/api/history', (req, res) => {
-  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2018-01-01&end=2019-06-25')
+  Axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2018-01-01&end=2019-06-25')
     .then((historicalData) => {
-      res.status(200).send(historicalData);
+      res.status(200).send(historicalData.data.bpi);
     })
     .catch((err) => {
       res.status(404).send(err);
