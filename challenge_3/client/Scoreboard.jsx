ea8cd4c55ca3a258da71ca738e0e4d83468d62d6
Scott Torres <scott.torres27@gmail.com>
2019-06-27 14:47:59 -0700

    Add 10th frame game logic.

--- challenge_3/client/Scoreboard.jsx
+++ challenge_3/client/Scoreboard.jsx
@@ -23,18 +23,20 @@ class Scoreboard extends React.Component {
       ],
       total: 0,
       currentTurn: 0,
-      isValidScore: true
+      isValidScore: true,
+      isGameOver: false,
     };
 
     this.handleScoreInput = this.handleScoreInput.bind(this);
     this.determineFrame = this.determineFrame.bind(this);
     this.determineNextTurn = this.determineNextTurn.bind(this);
+    this.checkGameStatus = this.checkGameStatus.bind(this);
   }
 
   handleScoreInput(nextScore) {
     let frame = this.determineFrame();
     let newBoard = this.state.player1;
-    if (frame[1] === 1 && (nextScore + newBoard[frame[0]][0] > 10)) {
+    if (this.state.currentTurn < 18 && frame[1] === 1 && (nextScore + newBoard[frame[0]][0] > 10)) {
       this.setState({
         isValidScore: false
       })
@@ -46,16 +48,22 @@ class Scoreboard extends React.Component {
         player1: newBoard,
         total: updatedTotal,
         currentTurn: nextTurn,
-        isValidScore: true
+        isValidScore: true,
+      }, () => {
+        this.checkGameStatus(); 
       });
     }
   }
 
   determineFrame() {
     let currentTurn = this.state.currentTurn;
-    let framePlacement = 1 + Math.floor(currentTurn / 2);
-    let ballPlacement = currentTurn % 2;
-    return [framePlacement, ballPlacement];
+    if (currentTurn === 20) {
+      return [10, 2];
+    } else {
+      let framePlacement = 1 + Math.floor(currentTurn / 2);
+      let ballPlacement = currentTurn % 2;
+      return [framePlacement, ballPlacement];
+    }
   }
 
   determineNextTurn(score) {
@@ -63,6 +71,17 @@ class Scoreboard extends React.Component {
       return this.state.currentTurn + 2;
     } else {
       return this.state.currentTurn + 1;
+    }
+  }
+
+  checkGameStatus() {
+    let currentTurn = this.state.currentTurn;
+    console.log(this.state.player1);
+    let tenthFrameScore = this.state.player1[10][0] + this.state.player1[10][1]
+    if (currentTurn > 19 && tenthFrameScore < 10) {
+      this.setState({
+        isGameOver: true,
+      })
     }
   }
 
@@ -87,6 +106,14 @@ class Scoreboard extends React.Component {
         <div>
           {!this.state.isValidScore && 
             <div>Not a valid score submission, total pins per frame cannot be greater than 10. Please re-input valid score.</div>
+          }
+        </div>
+        <div>
+          {this.state.isGameOver && 
+            <div>
+              <div>Nice game! Click "New Game" to start a new game!</div>
+              <button>New Game!</button>
+            </div>
           }
         </div>
       </div>
