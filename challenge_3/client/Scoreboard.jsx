a18151cb6f19a7b7f1ffbcd04aeaa65a0eb35c4c
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-27 11:36:44 -0700

    Create Scoreboard component.

---
+++ challenge_3/client/Scoreboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Scoreboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Scoreboard</div>
+    )
+  }
+}
+
+export default Scoreboard;
